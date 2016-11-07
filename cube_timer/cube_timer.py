277d6deed30f834214cd36ce416a5e99e88bc976
guillaume <guillaume-bideau@live.fr>
2016-11-07 20:27:23 +0100

    premier commit : rajout des projets

---
+++ cube_timer/cube_timer.py
@@ -0,0 +1,12 @@
+#! /usr/bin/pyhton2.7
+
+import time
+
+print("appuyez sur la touche enter :")
+depart=input()
+t0=time.time()
+
+fin=input()
+t1=time.time()
+
+print("Vous avez mis : ",t1-t0," s")
