a02da81dcdbc279d73bd21c4e81bca8fcbbc2bab
guillaume <guillaume-bideau@live.fr>
2016-11-07 21:14:12 +0100

    modification cube_timer

--- cube_timer/cube_timer.py
+++ cube_timer/cube_timer.py
@@ -1,12 +1,35 @@
-#! /usr/bin/pyhton2.7
+#! /usr/bin/python2.7
 
 import time
+import commands
 
-print("appuyez sur la touche enter :")
-depart=input()
-t0=time.time()
+continuer = True
+def save():
+    utilisateur=commands.getstatusoutput('ls ./utilisateurs')
+    print("les utilisateurs sont : ",utilisateur)
 
-fin=input()
-t1=time.time()
+while continuer == True :
 
-print("Vous avez mis : ",t1-t0," s")
+    print("appuyez sur la touche enter :")
+    depart = raw_input()
+    t0=time.time()
+
+    fin = raw_input()
+    t1=time.time()
+    print("Vous avez mis : ",t1-t0," s")
+
+    print("Voulez vous sauvegarder votre score ? y/n")
+    score = raw_input()
+    if (score == "y" or score == "Y" ) :
+        save_success=save()
+    else :
+        save = False
+    
+
+    print("Voulez vous rejouez ? y/n")
+    choix=raw_input()
+    if (choix == "y" or choix == "Y" ) :
+        continuer = True
+    else :
+        continuer = False
+
