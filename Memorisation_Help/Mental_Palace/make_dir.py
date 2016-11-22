6d53121d3f9aead70287f1d80d6a5d9a7aaf2ee4
reocedas <mougel.loic.pro@gmail.com>
2016-11-22 20:53:05 +0100

    finalisation du fichier makedir

--- Memorisation_Help/Mental_Palace/make_dir.py
+++ Memorisation_Help/Mental_Palace/make_dir.py
@@ -1,40 +1,95 @@
 import os
 
-
 def Make_direction():
     logname = os.environ['LOGNAME']
     verification = True
-    print "veuillez indiquer votre numero d'utilisateur:\n"
-    path = "/home/"+logname+"/projet_commun/Users_Files"
-    if os.path.isdir(path):
-        users = os.listdir(path)
-        users.append("New user")
-        print "Dossier d'utilisateurs existant"
-    else :
-        os.mkdir(path)
-        users = ["New user"]
-        print "Dossier d'utilisateurs cree"
+
+    if "projet_commun" in os.getcwd():
+        while "projet_commun" in os.getcwd():
+            os.chdir("..")
+        
+        path = os.getcwd()+"/projet_commun/Users_Files"
     
-    print users
+        print "veuillez indiquer votre numero d'utilisateur:\n"
+        if os.path.isdir(path):
+            users = os.listdir(path)
+            users.append("New user")
+            print "Dossier d'utilisateurs existant"
+        else :
+            os.mkdir(path)
+            users = ["New user"]
+            print "Dossier d'utilisateurs cree"
+    
+        print users
 
-    for i in users:
-        print users.index(i),":",i,"\n"
+        for i in users:
+            print users.index(i),":",i,"\n"
 
-    while verification:
-        try :
-            answer = input("numero :")
-            print answer, users[answer]
-            if users[answer] == 'New user':
-                dirname=raw_input("entrez le nom du nouvel utilisateur\n")
-                os.mkdir(path+"/"+dirname)
-                print("dossier cree")
-                os.chdir(path+"/"+dirname)
-                print("pwd ok")
-                verification = False
-            else : 
-                os.chdir(path+"/"+users[answer])
-                verification = False
-        except :
-            verification = True
-    user_path=path+"/"+users[answer]
+        while verification:
+            try :
+                answer = input("numero :")
+                print answer, users[answer]
+                if users[answer] == 'New user':
+                    dirname=raw_input("entrez le nom du nouvel utilisateur\n")
+                    os.mkdir(path+"/"+dirname)
+                    print("dossier cree")
+                    os.chdir(path+"/"+dirname)
+                    print("pwd ok")
+                    verification = False
+                else : 
+                    os.chdir(path+"/"+users[answer])
+                    verification = False
+            except :
+                verification = True
+        user_path=path+"/"+users[answer]
+    else:
+        if os.path.isfile(direction.log):
+          fichier = open("direction.log","r")
+          path = fichier.read()
+          path=path.lower
+
+        else : 
+            while verification:
+                try:
+                    path=raw_input("entrez un chemin valide pour la creaction de Users\n")
+                    os.chdir(path)
+                    verification = False
+                except:
+                    print "mauvaise entree"
+            path = path+"/Users_Files"
+            fichier = open("direction.log","w")
+            fichier.write(path)
+            os.mkdir(path)
+        
+        if os.path.isdir(path):
+            users = os.listdir(path)
+            users.append("New user")
+            print "Dossier d'utilisateurs existant"
+        else :
+            os.mkdir(path)
+            users = ["New user"]
+            print "Dossier d'utilisateurs cree"
+    
+        print users
+
+        for i in users:
+            print users.index(i),":",i,"\n"
+
+        while verification:
+            try :
+                answer = input("numero :")
+                print answer, users[answer]
+                if users[answer] == 'New user':
+                    dirname=raw_input("entrez le nom du nouvel utilisateur\n")
+                    os.mkdir(path+"/"+dirname)
+                    print("dossier cree")
+                    os.chdir(path+"/"+dirname)
+                    print("pwd ok")
+                    verification = False
+                else : 
+                    os.chdir(path+"/"+users[answer])
+                    verification = False
+            except :
+                verification = True
+        user_path=path+"/"+users[answer]
     return user_path 
--- Memorisation_Help/make_dir.py
+++ Memorisation_Help/make_dir.py
@@ -1,9 +1,9 @@
 import os
 
-
 def Make_direction():
     logname = os.environ['LOGNAME']
     verification = True
+
     if "projet_commun" in os.getcwd():
         while "projet_commun" in os.get_cwd():
             os.chdir("..")
@@ -43,7 +43,6 @@ def Make_direction():
                 verification = True
         user_path=path+"/"+users[answer]
     else:
-        verification = True
         if os.path.isfile(direction.log):
           fichier = open("direction.log","r")
           path = fichier.read()
@@ -57,5 +56,40 @@ def Make_direction():
                     verification = False
                 except:
                     print "mauvaise entree"
-            os.mkdir(
+            path = path+"/Users_Files"
+            fichier = open("direction.log","w")
+            fichier.write(path)
+            os.mkdir(path)
+        
+        if os.path.isdir(path):
+            users = os.listdir(path)
+            users.append("New user")
+            print "Dossier d'utilisateurs existant"
+        else :
+            os.mkdir(path)
+            users = ["New user"]
+            print "Dossier d'utilisateurs cree"
+    
+        print users
+
+        for i in users:
+            print users.index(i),":",i,"\n"
+
+        while verification:
+            try :
+                answer = input("numero :")
+                print answer, users[answer]
+                if users[answer] == 'New user':
+                    dirname=raw_input("entrez le nom du nouvel utilisateur\n")
+                    os.mkdir(path+"/"+dirname)
+                    print("dossier cree")
+                    os.chdir(path+"/"+dirname)
+                    print("pwd ok")
+                    verification = False
+                else : 
+                    os.chdir(path+"/"+users[answer])
+                    verification = False
+            except :
+                verification = True
+        user_path=path+"/"+users[answer]
     return user_path 
