ba01283b3bd43a5d6fbed343bdea8c8041dd8c4a
guillaume <guillaume-bideau@live.fr>
2016-11-25 13:11:46 +0100

    Modification majeur du cul de chouette il n'y a plus le coter orienter objet du programme mais les fonctions sont les meme par contre il y en a d'autre qui ont été créée

--- Cul_de_chouette/cul_de_chouette.py
+++ Cul_de_chouette/cul_de_chouette.py
@@ -7,19 +7,10 @@ import os
 import cul_de_chouette_function as fct
 import cul_de_chouette_objet as obj
 
-"""
-Je ne sais pas pourquoi je ne peux pas faire passer des Booleen en argument pour la methode de Chouette_velute
-
-Et rajouter les autres regles et ensuite faire un truc qui permet de les prendre en compte ou pas genre 
-Par defaut on active tout et
-rentrer les regles que vous ne voulez pas
-a = :_
-False regle de a
-et apres quand on regarde si sa fait un truc ou pas on instantie que les trucs true.
-"""
 Jouer = True
 Nouvelle_partie = True
 Premiere_partie = True
+Gagnant = False
 while Jouer == True :
     if Nouvelle_partie == True and Premiere_partie == True:
         nombre_de_joueur = input("entrer le nombre de joueur s'il vous plait (max 4): \n")
@@ -27,52 +18,84 @@ while Jouer == True :
         while nombre_de_joueur_a_creer > 0 :
             joueur_numero = nombre_de_joueur-nombre_de_joueur_a_creer +1
             if joueur_numero == 1:
-                nom = raw_input('rentrer votre nom joueur 1 : ')
-                joueur_1 = obj.Joueur(nom,0)
+                nom_joueur_1 = raw_input('rentrer votre nom joueur 1 : ')
+                score_joueur_1 = 0
             elif joueur_numero == 2:
-                nom = raw_input('rentrer votre nom joueur 2 : ')
-                joueur_2 = obj.Joueur(nom,0)
+                nom_joueur_2 = raw_input('rentrer votre nom joueur 2 : ')
+                score_joueur_2 = 0
             elif joueur_numero == 3:
-                nom = raw_input("rentrer votre nom joueur 3 : ")
-                joueur_3 = obj.Joueur(nom,0)
+                nom_joueur_3 = raw_input("rentrer votre nom joueur 3 : ")
+                score_joueur_3 = 0
             elif joueur_numero == 4:
-                nom = raw_input("rentrer votre nom joueur 4 : ")
-                joueur_4 = obj.Joueur(nom,0)
+                nom_joueur_4 = raw_input("rentrer votre nom joueur 4 : ")
+                score_joueur_4 = 0
             else :
                 print("Pas plus de 4 joueurs !!")
             nombre_de_joueur_a_creer = nombre_de_joueur_a_creer -1 
-        Premiere_partie = False
+
+    Premiere_partie = False
+    is_gagnant = False
+        
     nb_etape = nombre_de_joueur
     joueur_actuelle = 1
     while nb_etape > 0 :
+        nombre_de_joueur_a_afficher = nombre_de_joueur
+#affiche les scores des joueurs au debut de leur tour
+        while nombre_de_joueur_a_afficher > 0 :
+                joueur_numero = nombre_de_joueur-nombre_de_joueur_a_afficher +1
+                if joueur_numero == 1:
+                    print("Les scores sont ",nom_joueur_1," : ",score_joueur_1,"\n")
+                elif joueur_numero == 2:
+                    print("Les scores sont ",nom_joueur_2," : ",score_joueur_2,"\n")
+                elif joueur_numero == 3:
+                    print("Les scores sont ",nom_joueur_3," : ",score_joueur_3,"\n")
+                elif joueur_numero == 4:
+                    print("Les scores sont ",nom_joueur_4," : ",score_joueur_4,"\n")
+                else :
+                    print("erreur ! ")
+                    exit()
+                nombre_de_joueur_a_afficher = nombre_de_joueur_a_afficher -1 
+                
         if joueur_actuelle == 1 :
-            nom = obj.Joueur.__affichage__(joueur_1)
+            nom = nom_joueur_1
             print("C'est au tour de",nom," de jouer ! ")
-            fct.Tour_de_jeu(joueur_1)
+            point = fct.Tour_de_jeu()
+            score_joueur_1 = score_joueur_1 + point
+            is_gagnant = fct.Gagnant(score_joueur_1)
+            if is_gagnant :
+                print("Le joueur ",nom_joueur_1," a gagner avec :", score_joueur_1)
+        
         elif joueur_actuelle == 2 :
-            nom = obj.Joueur.__affichage__(joueur_2)
+            nom = nom_joueur_2
             print("C'est au tour de",nom," de jouer ! ")
-            fct.Tour_de_jeu(joueur_2)
+            point = fct.Tour_de_jeu()
+            score_joueur_2 = score_joueur_2 + point
+            is_gagnant = fct.Gagnant(score_joueur_2)
+            if is_gagnant :
+                print("Le joueur ",nom_joueur_2," a gagner avec :", score_joueur_2)
+        
         elif joueur_actuelle == 3 :
-            nom = obj.Joueur.__affichage__(joueur_3)
+            nom = nom_joueur_3
             print("C'est au tour de",nom," de jouer ! ")
-            fct.Tour_de_jeu(joueur_3)
+            point = fct.Tour_de_jeu()
+            score_joueur_3 = score_joueur_3 + point
+            is_gagnant = fct.Gagnant(score_joueur_3)
+            if is_gagnant :
+                print("Le joueur ",nom_joueur_3," a gagner avec :", score_joueur_3)
+            
         elif joueur_actuelle == 4 :
-            nom = obj.Joueur.__affichage__(joueur_4)
-            #print('C\' est au tour de',nom,' de jouer ! \n il a ',score,'point')
+            nom = nom_joueur_4
             print('C\' est au tour de',nom,' de jouer !')
-            fct.Tour_de_jeu(joueur_4)
+            point = fct.Tour_de_jeu()
+            score_joueur_4 = score_joueur_4 + point
+            is_gagnant = fct.Gagnant(score_joueur_4)
+            if is_gagnant :
+                print("Le joueur ",nom_joueur_4," a gagner avec :", score_joueur_4)
+                
         joueur_actuelle=joueur_actuelle+1
+        print(joueur_actuelle)
         nb_etape = nb_etape-1
-"""
-    elif Nouvelle_partie == True and Premier_partie == False :
-        choix = raw_input("Voulez vous garder les memes joueurs ? Oui ou Non \n")
-        if choix == O or choix == o:
-            print("Vous garder les meme joueurs, bonne partie ;)")
-#remettre les scores a zero
-        else :
-#y faudrai surement detruire les objets joueur de la partie d'avant
-            nombre_de_joueur = fct.Creation_partie()
-"""
-#verifiation s il y a un gagnant
-
+        print(nb_etape)
+        print(is_gagnant)
+    
+    #verifiation s il y a un gagnant
--- Cul_de_chouette/cul_de_chouette_function.py
+++ Cul_de_chouette/cul_de_chouette_function.py
@@ -34,40 +34,108 @@ def Verificateur_de_des(de) :
     return is_ok
 
 
-def is_figure(is_a_chouette_velute, is_a_chouette, is_a_velute):
-    selecteur = 0
-    if is_a_chouette_velute == True :
-        print("is a chouette : ",is_a_chouette)
-        print("is a velute : ",is_a_velute)
-        print("is a chouette velute : ++ ",is_a_chouette_velute)
-        selecteur = 3
-    elif is_a_chouette == True:
-        print("is a chouette ++ : ",is_a_chouette)
-        print("is a velute : ",is_a_velute)
-        print("is a chouette velute : ",is_a_chouette_velute)
-        selecteur = 1
-    elif is_a_velute == True :
-        print("is a chouette : ",is_a_chouette)
-        print("is a velute ++ : ",is_a_velute)
-        print("is a chouette velute : ",is_a_chouette_velute)
-        selecteur = 2
-    else :
-        print("Pas de point marquer !!")
-        selecteur = 0
-    return selecteur
+#def is_figure(is_a_chouette_velute, is_a_chouette, is_a_velute):
+    #selecteur = 0
+    #if is_a_chouette_velute == True :
+        #print("is a chouette : ",is_a_chouette)
+        #print("is a velute : ",is_a_velute)
+        #print("is a chouette velute : ++ ",is_a_chouette_velute)
+        #selecteur = 3
+    #elif is_a_chouette == True:
+        #print("is a chouette ++ : ",is_a_chouette)
+        #print("is a velute : ",is_a_velute)
+        #print("is a chouette velute : ",is_a_chouette_velute)
+        #selecteur = 1
+    #elif is_a_velute == True :
+        #print("is a chouette : ",is_a_chouette)
+        #print("is a velute ++ : ",is_a_velute)
+        #print("is a chouette velute : ",is_a_chouette_velute)
+        #selecteur = 2
+    #else :
+        #print("Pas de point marquer !!")
+        #selecteur = 0
+    #return selecteur
 
-def Tour_de_jeu(joueur):
-    is_a_chouette = False
-    is_a_velute = False
-    is_a_chouette_velute = False
+def Chouette(de_1,de_2,de_3):
+    if de_1 == de_2 or de_1 == de_3 :
+        chouette_de = de_1
+    elif de_2 == de_3 :
+        chouette_de = de_2
+    else :
+        chouette_de = int(0)
+    return chouette_de
+
+def Cul_de_chouette(de_1,de_2,de_3):
+    if de_1 == de_2 and de_2 == de_3 :
+        is_cul_de_chouette = de_3
+    else :
+        is_cul_de_chouette = False
+    return is_cul_de_chouette
+
+def Chouette_bleu_rouge(de_1,de_2,de_3):
+    if (de_1 == 4 and de_2 == 4 and de_3 == 5) or (de_1 == 5 and de_2 == 4 and de_3 == 4) or (de_1 == 4 and de_2 == 5 and de_3 == 4):
+        bleu_rouge = True
+    else :
+        bleu_rouge = False
+    return bleu_rouge
+
+def Velute(de_1,de_2,de_3):
+    if de_1 + de_2 == de_3 :
+        velute_de = de_3
+    elif de_1 + de_3 == de_2 :
+        velute_de = de_2
+    elif de_2 + de_3 == de_1 :
+        velute_de = de_1
+    else :
+        velute_de = False
+    return velute_de
+
+
+def Figure_and_points(de_1,de_2,de_3):
+    points = 0
+    is_chouette = Chouette(de_1,de_2,de_3)
+    is_velute = Velute(de_1,de_2,de_3)
+    if is_chouette :
+        is_bleu_rouge = Chouette_bleu_rouge(de_1,de_2,de_3)
+        is_chouette_velute = Velute(de_1,de_2,de_3)
+        is_cul_de_chouette = Cul_de_chouette(de_1,de_2,de_3)
+        #print(is_bleu_rouge)
+        #print(is_chouette_velute)
+        if is_bleu_rouge :
+            points = is_chouette**2
+#faire les trucs du bleu rouge
+#faire le condition pour les autres chouette special
+        elif is_chouette_velute :
+            points = 2*(is_chouette**2)
+            print("Vous avez gagner les points de la chouette velute")
+        elif is_cul_de_chouette : 
+            points = 50+(is_cul_de_chouette-1)*10
+        else :
+            points = is_chouette**2
+            print("Vous avez gagner les points de la chouette de ",is_chouette)
+    elif is_velute :
+        #print(is_velute)
+        points = 2*(is_velute**2)
+        print("Vous avez gagner les points de la velute")
+    return points
+
+def Tour_de_jeu():
+    #is_a_chouette = False
+    #is_a_velute = False
+    #is_a_chouette_velute = False
     de_1 = Lancer_de_de(1)
     de_2 = Lancer_de_de(2)
     de_3 = Lancer_de_de(3)
-    test_chouette = obj.Chouette(de_1, de_2, de_3)
-    test_velute = obj.Velute(de_1, de_2, de_3)
-    is_a_chouette = test_chouette.__is_Chouette__()
-    is_a_velute = test_velute.__is_Velute__()
-    test_chouette_velute=obj.Chouette_Velute()
-    is_a_chouette_velute=test_chouette_velute.__is_Chouette_Velute__(is_a_chouette, is_a_velute)
-    selecteur_de_figure=is_figure(is_a_chouette_velute,is_a_chouette,is_a_velute)
-    print(selecteur_de_figure)
+    de_1 = int(de_1)
+    de_2 = int(de_2)
+    de_3 = int(de_3)
+    point = Figure_and_points(de_1,de_2,de_3)
+    return point
+
+def Gagnant(score) :
+    score = int(score)
+    if score >= 343 :
+        is_gagnant = True
+    else :
+        is_gagnant = False
+    return is_gagnant
