7c69d02635122ecc2fe3d5139552112b779fdccc
reocedas <mougel.loic@gmail.com>
2016-12-02 14:33:25 +0100

    Finalisation du programme Table de caracter et ajout de certains fichier image

--- Memorisation_Help/Table_de_caractere/table_de_caractere.py
+++ Memorisation_Help/Table_de_caractere/table_de_caractere.py
@@ -5,69 +5,85 @@ import threading
 from scipy import ndimage
 import matplotlib.pyplot as plt
 
+global user_path
+global path_to_diec
+global index_max
+global index_min
+verification = True
+while verification :
+    try :
+        index_min = input("Entrer la valeur minimale\n")
+        index_max = input("Entrer la valeur maximale\n")
+        verification = False
+    except:
+        verification = True
+
 class Pic_Thread(threading.Thread):
+    
     def __init__(self):
         threading.Thread.__init__(self)
         self.Stop=False
+        self.pic_index=0
+        self.i=0
+
     def run(self):
-        try :
-            self.fichier = user_path + "/" + str(index)+".png"
-            self.pic = ndimage.imread(self.fichier)
-            plt.imshow(self.pic)
-            plt.show()
-            while True:
-                if self.Stop:
-                    raise ValueError ("EOT")
-        except :
-            print ""
+        self.set_pic_index()
+        self.fichier = user_path + "/" + str(self.pic_index)+".png"
+        self.pic = ndimage.imread(self.fichier)
+        plt.imshow(self.pic)
+        plt.show()
+        while not self.Stop:
+            self.i+=1
+    
+    def update(self):
+        self.set_pic_index()
+        self.fichier = user_path + "/" + str(self.pic_index)+".png"
+        self.pic = ndimage.imread(self.fichier)
+        plt.imshow(self.pic)
+        plt.show()
+
+    def get_pic_index(self):
+        return self.pic_index
+
+    def set_pic_index(self):
+        self.pic_index = np.random.random_integers(index_min,index_max)
+    
     def stop(self):
         self.Stop = True
-    def stop_and_quit(self):
-        exit()
 
 
-global user_path
-global path_to_diec
-global index
 
 user_path,path_to_dic = make_dir.Make_direction()
 
 running = True
+picture = Pic_Thread() 
+picture.start()
 
 while running:
-    index = np.random.random_integers(0,3)
-    picture = Pic_Thread() 
-    picture.start()
     verification = True
 
     while verification:
         try : 
-            answer = raw_input("Quel est le nombre correspondant?")
-       
+            answer = raw_input("Quel est le nombre correspondant?\n")
             if answer.lower() == "exit":
                 verification = False
                 running = False
+                picture.stop()
                 print "END"
                 break
 
-            elif int(answer) == index :
+            elif int(answer) == picture.get_pic_index() :
                 print "CORRECT.\n"
                 verification = False
-                picture.stop()
-
+                picture.update()
             else : 
-                print "FALSE. Bonne reponse : ", index, "\nAppuyez sur enter\n"
+                print "FALSE. Bonne reponse : ", get_pic_index, "\nAppuyez sur enter\n"
                 end=raw_input()       
                 verification = False
-                picture.stop()
+                picture.update()
         except :
             verification = True
 
     if answer.lower() == "exit":
         break
-    else:
-        picture = 0
-plt.close()
-picture.stop_and_quit()
-print "End"
-exit()
+print "Fermer la fenetre d'image avec ctrl + w"
