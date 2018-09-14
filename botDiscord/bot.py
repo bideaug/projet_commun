dbbebdb5919b0b649c5f87fa6733dd92cc260763
reocedas <mougel.loic@gmail.com>
2018-09-14 16:37:48 +0200

    Updates

--- botDiscord/bot.py
+++ botDiscord/bot.py
@@ -9,17 +9,22 @@ import numpy as np
 from discord import Game
 from discord.ext.commands import Bot
 
+log_file = np.genfromtxt(os.environ['HOME']+'/dat.log',dtype=str)
+print (log_file)
 BOT_PREFIX = ("?", "!")
-TOKEN = 
+
+TOKEN = log_file[0] 
+FAIM = log_file[1]
+TIME = log_file[0]
+
 
 class Duck_bot(Bot):
-    def __init__(self,command_prefix):
+    def __init__(self,command_prefix,faim = 100,time = None):
         Bot.__init__(self,command_prefix)
-        self.faim = 100
-        self.intern_clock = 0
-        self.faim_time = 1
-        
-    
+        self.faim = faim
+        self.intern_clock = time
+        self.faim_time = 5
+         
     def get_faim(self):
         return self.faim
 
@@ -43,47 +48,57 @@ async def on_message(message):
         msg = 'Hello {0.author.mention}! <3'.format(message)
         await message.channel.send(msg)
         
-    if 'ducky' in str(message.content).lower():
+    elif 'ducky' in str(message.content).lower():
         msg = 'Coin?'.format(message)
         await message.channel.send(msg)
         
-    if 'faim' in str(message.content).lower():
+    elif 'faim' in str(message.content).lower():
         msg = ('FAIM! genre '+str(bot.get_faim()))+"%".format(message)
         await message.channel.send(msg)
     
-    if 'snack' in str(message.content).lower():
+    elif 'snack' in str(message.content).lower():
         possible_responses = ['Naaaaa',
                             'Coin...',
                             'Bofbof!',
                             'Miam!',
+                            'Très!',
                             'Owiiiiiii',
                             ]
-        msg = possible_responses[int(0.5+(4*bot.get_faim()/100))]
+        msg = possible_responses[int(0.5+((len(possible_responses)-1)*bot.get_faim()/100))]
         await message.channel.send(msg)
     await bot.process_commands(message)
 
 
-@bot.event
-async def start_faim():
+@bot.command()
+async def start_faim(ctx):
     
     time_ref = time.localtime()[4]
+    print(time.localtime()[4])
     if time_ref == 0:
-        time.sleep(60)
+        asyncio.sleep(60)
         time_ref = time.localtime()[4]
         
     while True:
-        if np.abs(time_ref-time.local_time()[4])>=bot.faim_time:
+        if np.abs(time_ref-time.localtime()[4])>bot.faim_time and bot.get_faim()<100:
             time_ref = time.localtime()[4]
             bot.set_faim(bot.get_faim()+1)
-        time.sleep(1)
-        await time.sleep(1)
+        await asyncio.sleep(10)
 
 
 @bot.command()
-async def snack(ctx):
-    bot.set_faim(bot.get_faim()-1)
-    await ctx.send("*scrounch scrouch*")
+async def snack(ctx, a : int = 1):
+    if a>5:
+        msg = "Arblaghbla...!"
+        await ctx.send(msg):
+    else:        
+        bot.set_faim(bot.get_faim()-a)
+        await ctx.send("*scrounch scrouch*")
 
+@bot.command()
+async def meal(ctx):
+    
+    bot.set_faim(bot.get_faim()+50)
+    await ctx.send("*Miam!*")
 
 @bot.command()
 async def pict(ctx):
@@ -112,6 +127,6 @@ async def on_ready():
     print(bot.user.name)
     print(bot.user.id)
     print('------')
-    bot.start_faim()
+    
 
 bot.run(TOKEN)
